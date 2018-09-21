136465fd6c7376123cd3669143742aeb5d803a04
debora.barbosa <debora.barbosa@est41017.mctic.gov.br>
2018-09-21 16:19:15 -0300

    disponibilizar

--- src/main/java/br/gov/mctic/sisgesac/automacao/pageobject/CadastrarSolicitacaoDePontoDePresencaPage.java
+++ src/main/java/br/gov/mctic/sisgesac/automacao/pageobject/CadastrarSolicitacaoDePontoDePresencaPage.java
@@ -1,7 +1,19 @@
 package br.gov.mctic.sisgesac.automacao.pageobject;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 import br.gov.mctic.sisgesac.automacao.core.AbstractPageObject;
+import br.gov.mctic.sisgesac.automacao.core.WDS;
 
 public class CadastrarSolicitacaoDePontoDePresencaPage extends AbstractPageObject {
+	
+	
+	public void campoObrigatorio(String campo) {
+		WebElement validarMensagem = WDS.get().findElement(By.xpath("//div[@input-name='+'campo+']"));
+		
+		
+			
+	}
 
 }
